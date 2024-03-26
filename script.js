8bb96bb70ee7b966be588c22c032e5f861efe834
Lucas <lucashenriquedml@gmail.com>
2024-03-26 10:51:23 -0300

    Add grid custom size and css on grid-item
    Using flex box and dividing the container size for
    the size of the grid i can always mantein the container size
    the same

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const container =  document.querySelector('.container');
 
+const gridSize = 5;
+
 const createGrid = (gridAmount) => {
   for (let i = 0; i < gridAmount; i++) {
     const row =  document.createElement('div');  
     row.classList.add('grid-row');
 
     for (let j = 0; j < gridAmount; j++) {
-      const widthAndHeight = 960 / gridSize; 
+      const widthAndHeight = 560 / gridSize; 
       const gridItem =  document.createElement('div');
       gridItem.classList.add('grid-item');
-      gridItem.style.height = `${widthAndHeight}`;
-      gridItem.style.width = `${widthAndHeight}`;
+      gridItem.style.height = `${widthAndHeight}px`;
+      gridItem.style.width = `${widthAndHeight}px`;
       row.appendChild(gridItem)
     }
     
@@ -18,3 +20,5 @@ const createGrid = (gridAmount) => {
   }
   
 }
+
+createGrid(gridSize);
