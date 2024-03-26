ab4b83472f21579b5bc6559832af8bc817ded134
Lucas <lucashenriquedml@gmail.com>
2024-03-26 13:24:53 -0300

    Make the reset button work properly
    Replacing the container child and using the user size
    i can calculate the right container size for the grid
    and the user can choose a value between 1 and 100

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const container =  document.querySelector('.container');
-
-const gridSize = 10;
+const container = document.querySelector(".container");
+const resetButton = document.querySelector("button");
+const gridSize = 16;
 
 const createGrid = (gridAmount) => {
+  container.replaceChildren();
+
   for (let i = 0; i < gridAmount; i++) {
-    const row =  document.createElement('div');  
-    row.classList.add('grid-row');
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
 
     for (let j = 0; j < gridAmount; j++) {
-      const widthAndHeight = 560 / gridSize; 
-      const gridItem =  document.createElement('div');
-      gridItem.classList.add('grid-item');
+      const widthAndHeight = 560 / gridAmount;
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
       gridItem.style.height = `${widthAndHeight}px`;
       gridItem.style.width = `${widthAndHeight}px`;
-      
-      gridItem.addEventListener('mouseenter', () => {
-        gridItem.style.backgroundColor = '#d65d0e'
-      })
-      row.appendChild(gridItem)
+
+      gridItem.addEventListener("mouseenter", () => {
+        gridItem.style.backgroundColor = "#d65d0e";
+      });
+      row.appendChild(gridItem);
     }
-    
-    container.appendChild(row)
+
+    container.appendChild(row);
   }
-  
-}
+};
 
 createGrid(gridSize);
+
+resetButton.addEventListener("click", () => {
+  let userSize = Number(prompt("Choose a grid size between 1 and 100"));
+  if (userSize >= 1 && userSize <= 100) {
+    createGrid(userSize);
+  } else {
+    alert("Error: Choosea number between 1 and 100");
+  }
+});
